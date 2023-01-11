bc3f76c5523b23d935877af1ae4ede86760b05ff
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-01-11 14:03:51 +0600

    выполнил дз 2

---
+++ DZ2_Erjan_22-01/app.js
@@ -0,0 +1,26 @@
+// 1 часть
+ var arr = [10, 20, 30, 50, 235, 3000]
+ console.log(arr["0"], arr["1"], arr["3"]);
+// 2 часть
+//   let num = 20;
+//   while (num >= 0) {
+//     console.log(num);
+//     num--;
+//   }
+// for (let num = 20; num >= 0; num--) {
+//     console.log(num);
+// }
+// 3 часть
+// var svetovor = prompt('Выберите цвет светофора')
+// if (svetovor==='зеленый'){
+//     alert('идти')
+// }
+// else if (svetovor==='желтый'){
+//     alert('жди')
+// }
+// else if (svetovor==='красный'){
+//     alert('стой')
+// }
+// else {
+//     alert('надо вводить цвета')
+// }
